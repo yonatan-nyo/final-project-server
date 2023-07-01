8fdca71a068842ba3b720f80c2642e0894cc161a
yonatan-nyo <yonatan.njoto@gmail.com>
2023-07-01 14:52:29 +0700

    added testing business

--- services/businesses-4002/__test__/index.test.js
+++ services/businesses-4002/__test__/index.test.js
@@ -1,9 +1,23 @@
 const request = require("supertest");
 const app = require("../app");
 const { mongoConnect, mongoClose } = require("../config/connectionMongoDB");
+const Bussiness = require("../models/bussinesses");
+
+const initializeBussiness = {
+  name: "",
+  slug: "",
+  overview: "",
+  brandUrl: "",
+  imagesUrl: "",
+  locations: "",
+  pdfUrl: "",
+  fundNeeded: "",
+  UserId: "",
+};
 
 beforeAll(async () => {
   await mongoConnect();
+  Bussiness.createBussiness(initializeBussiness);
 });
 
 afterAll(async () => {
@@ -11,10 +25,21 @@ afterAll(async () => {
 });
 
 describe("Bussinesses", () => {
-  it("get /bussinesses", async () => {
+  it("GET /bussinesses", async () => {
     const response = await request(app).get("/bussinesses").set("Accept", "application/json");
     console.log(response);
 
     expect(response.status).toBe(200);
+    expect(response.body).toEqual([initializeBussiness]);
+  });
+  it("GET /bussinesses/:slug", async () => {
+    // const response = await request(app).get("/bussinesses").set("Accept", "application/json");
+    // console.log(response);
+    // expect(response.status).toBe(200);
+  });
+  it("POST /bussinesses/", async () => {
+    // const response = await request(app).get("/bussinesses").set("Accept", "application/json");
+    // console.log(response);
+    // expect(response.status).toBe(200);
   });
 });
